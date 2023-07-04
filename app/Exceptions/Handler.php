568c89eb1d781aec8e4d6eb8891bb69d381e2ce9
Pratish Joshi <pratish.joshi@krafters.dev>
2023-07-05 00:25:17 +0545

    Added a bit more of exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,17 @@
 namespace App\Exceptions;
 
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
-use Ramsey\Uuid\Exception\BuilderNotFoundException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
+use Illuminate\Contracts\Encryption\EncryptException;
+use Illuminate\Contracts\Encryption\DecryptException;
 use App\Traits\JSONResponseTrait;
 
 class Handler extends ExceptionHandler
@@ -34,7 +38,7 @@ class Handler extends ExceptionHandler
     {
         $this->renderable(function (NotFoundHttpException $exception, $request) {
             if ($request->is('api/*')) {
-                return $this->successAndErrorResponse(404, null, 'Route not found', null);
+                return $this->successAndErrorResponse(404, null, null, ['resource' => 'Resource not found']);
             }
         });
     }
@@ -50,13 +54,38 @@ class Handler extends ExceptionHandler
         }
 
         if ($exception instanceof AuthenticationException) {
-            return $this->successAndErrorResponse(422, null, null, ['authentication' => 'Unauthenticated']);
+            return $this->successAndErrorResponse(422, null, null, ['authentication' => 'Invalid credentials']);
         }
 
         if ($exception instanceof RouteNotFoundException) {
             return $this->successAndErrorResponse(404, null, null, ['route' => 'Route not found']);
         }
 
+        if ($exception instanceof QueryException) {
+            return $this->successAndErrorResponse(500, null, null, ['database' => 'Database error']);
+        }
+
+        if ($exception instanceof FileNotFoundException) {
+            return $this->successAndErrorResponse(404, null, null, ['not_found' => 'File not found']);
+        }
+
+        if ($exception instanceof EncryptException) {
+            return $this->successAndErrorResponse(422, null, null, ['encrypt' => 'Encryption error']);
+        }
+
+        if ($exception instanceof DecryptException) {
+            return $this->successAndErrorResponse(422, null, null, ['decrypt' => 'Decryption error']);
+        }
+
         return parent::render($request, $exception);
+    }
+
+    public function report(Throwable $exception)
+    {
+        if ($this->shouldReport($exception)) {
+            Log::error($exception->getMessage());
+        }
+
+        parent::report($exception);
     }
 }
